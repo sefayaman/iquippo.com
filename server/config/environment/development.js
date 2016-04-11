02515a6bbbdb0bdb7c34dc6e7a0b9c53670c3811
Ritin <ritinpali@gmail.com>
2016-04-11 10:17:26 +0530

    changes in db url

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://127.0.0.1/sreidubaidb'
+    uri: 'mongodb://localhost/sreiglobaldb'
   },
   uploadPath: 'client/assets/uploads/',
   serverPath: "http://192.168.14.120:8100",
