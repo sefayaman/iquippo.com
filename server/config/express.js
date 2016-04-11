9edfe72c70341785cebf6530173890784c90953c
Ritin <ritinpali@gmail.com>
2016-04-11 16:01:12 +0530

    code change for 404 error

--- server/config/express.js
+++ server/config/express.js
@@ -46,4 +46,18 @@ module.exports = function(app) {
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
+
+  app.head('*', function(req, res, next){
+      res.status(501).end();
+  });
+  app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method == 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next();
+    }
+  });
 };
