5754dacce0994025b5b3e35d2051cde086c27a22
Ritin <ritinpali@bookawheel.com>
2016-05-05 15:40:21 +0530

    compare on cart page

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -4,11 +4,13 @@
 
 angular.module('sreizaoApp').controller('ViewCartCtrl',ViewCartCtrl)
 
-function ViewCartCtrl($scope,$rootScope,cartSvc,Auth,Modal) {
+function ViewCartCtrl($scope,$rootScope,cartSvc,Auth,Modal,$uibModal) {
     var vm = this;
+    vm.productListToCompare = [];
     vm.deleteProductFromCart = deleteProductFromCart;
     vm.clearCart = clearCart;
-
+    vm.updateSelection = updateSelection;
+    vm.compare = compare;
     cartSvc.getCartData(Auth.getCurrentUser()._id);
     
     function deleteProductFromCart(index){
@@ -28,11 +30,15 @@ function ViewCartCtrl($scope,$rootScope,cartSvc,Auth,Modal) {
     }; 
 
     function deleteFn(index){
-         $rootScope.cart.products.splice(index,1);
+      var prd = $rootScope.cart.products[index]; 
+      $rootScope.cart.products.splice(index,1);
       cartSvc.updateCart($rootScope.cart)
         .success(function(res){
             Modal.alert(informationMessage.deleteCartProductSuccess,true);
             $rootScope.cartCounter = $rootScope.cart.products.length;
+            var idx = getIndex(prd);
+            if(idx != -1)
+              vm.productListToCompare.splice(idx,1);
         })
         .error(function(res){
              Modal.alert(res,true);
@@ -45,11 +51,56 @@ function ViewCartCtrl($scope,$rootScope,cartSvc,Auth,Modal) {
         .success(function(res){
             Modal.alert(informationMessage.clearCartSuccess,true);
             $rootScope.cartCounter = $rootScope.cart.products.length;
+            vm.productListToCompare = [];
         })
         .error(function(res){
              Modal.alert(res,true);
         })
     }
+  function compare(){
+
+     if(vm.productListToCompare.length < 2){
+          Modal.alert("Please select atleat two products to compare.",true);
+          return;
+      }
+       var prevScope = $rootScope.$new();
+       prevScope.productList = vm.productListToCompare;
+       prevScope.uploadImagePrefix = $rootScope.uploadImagePrefix;     
+       var prvProductModal = $uibModal.open({
+            templateUrl: "app/product/productcompare.html",
+            scope: prevScope,
+            windowTopClass:'product-preview',
+            size: 'lg'
+        });
+         prevScope.dismiss = function () {
+          prvProductModal.dismiss('cancel');
+        };
+  }
+
+  function updateSelection(event,prd){
+        var checkbox = event.target;
+        var action = checkbox.checked?'add':'remove';
+        if( action == 'add' && vm.productListToCompare.length >= 4){
+          angular.element(checkbox).prop("checked",false);
+          Modal.alert("You can compare upto 4 products.",true);
+          return;
+        }
+        var index = getIndex(prd);
+        if(action == 'add' && index == -1)
+          vm.productListToCompare.push(prd)
+        if(action == 'remove' && index != -1)
+          vm.productListToCompare.splice(index,1);
+  }
+
+  function getIndex(prd){
+    var index = -1;
+    vm.productListToCompare.forEach(function(item,idx){
+      if(item._id == prd._id)
+        index = idx;
+    });
+    return index;
+  }
+
 }
 
 angular.module('sreizaoApp').factory("cartSvc",['$http','$rootScope',function($http,$rootScope){
