81fdc7a1ce60e56298b475099ded97df452343c9
ritin <ritinpali@gmail.com>
2016-05-21 17:17:13 +0530

    bug fixes

--- client/app/product/productlisting.controller.js
+++ client/app/product/productlisting.controller.js
@@ -104,8 +104,8 @@ function searchFilter(type)
 function searchUtil(item,toSearch, type)
 {
   if(type == 'assetId'){
-    assetIdArr = toSearch.split(",");
-    if(!assetIdArr || assetIdArr.length > 0)
+    var assetIdArr = toSearch.split(",");
+    if(!assetIdArr || assetIdArr.length == 0)
       return;
     return ( assetIdArr.indexOf(item.assetId) != -1) ? true : false ;
   } else if(type == 'tradeType'){
