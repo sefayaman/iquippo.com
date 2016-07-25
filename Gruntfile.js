0a1c310411464bc98bebef6c45ed9e95065e4759
Ritin <ritinpali@gmail.com>
2016-07-25 16:45:22 +0530

    code reverted

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 9010
+        port: process.env.PORT || 8100
       },
       dev: {
         options: {
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
         options: {
           nodeArgs: ['--debug-brk'],
           env: {
-            PORT: process.env.PORT || 9010
+            PORT: process.env.PORT || 8100
           },
           callback: function (nodemon) {
             nodemon.on('log', function (event) {
