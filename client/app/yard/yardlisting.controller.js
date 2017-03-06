36b14ca3ba75010669c0791dc67e99f4693a9993
Atul Agrawal <atul.agrawal@bharatconnect.com>
2017-03-06 12:00:28 +0530

    yardlisting ctrl

---
+++ client/app/yard/yardlisting.controller.js
@@ -0,0 +1,11 @@
+(function(){
+'use strict';
+angular.module('yard').controller('YardListingCtrl',YardListingCtrl);
+
+function YardListingCtrl($scope, $location, $rootScope, $http, productSvc, classifiedSvc, Modal, DTOptionsBuilder, $uibModal, $state, Auth, notificationSvc,uploadSvc,$timeout,$stateParams) {
+	var vm  = this;
+	$scope.isCollapsed = true;
+ 
+}
+
+})();
