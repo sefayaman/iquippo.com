65d4e554cbfbde0125bd29e1de7f55f4e3fc317f
Atul Agrawal <atul.agrawal@bharatconnect.com>
2017-03-21 22:40:19 +0530

    Minor bug fix

--- client/app/product/product.controller.js
+++ client/app/product/product.controller.js
@@ -715,7 +715,7 @@ angular.module('sreizaoApp').controller('CropImageCtrl', CropImageCtrl);
           ret = true;
       }
       if($scope.product.tradeType != "SELL"){
-        if(!$scope.product.rent.negotiable && angular.isUndefined($scope.product.rent.rateHours) && angular.isUndefined($scope.product.rent.rateDays) && angular.isUndefined($scope.product.rent.rateMonths)) {
+        if($scope.product.rent && !$scope.product.rent.negotiable && angular.isUndefined($scope.product.rent.rateHours) && angular.isUndefined($scope.product.rent.rateDays) && angular.isUndefined($scope.product.rent.rateMonths)) {
           ret = true;
           Modal.alert("Please select at-least one check box in 'Check Rental Rate For'.",true);
           return;
