aa8d2819868204771bae552feb989e627e7b3b15
subhash <subhash.patel@bharatconnect.com>
2017-06-28 15:11:26 +0530

    Changes for Task T271 T242 T266
    Summary: Changes for Task T271 T242 T266
    
    Test Plan: NA
    
    Reviewers: narendra.khatri
    
    Reviewed By: narendra.khatri
    
    Maniphest Tasks: T271
    
    Differential Revision: http://dev.varaunited.net/D65

--- server/api/enterprise/enterprise.controller.js
+++ server/api/enterprise/enterprise.controller.js
@@ -597,6 +597,10 @@ exports.bulkUpload = function(req, res) {
     }
 
     function validateMasterData(callback){
+        if(!row.brand){
+          delete row.model;
+          return callback();
+        }
        commonFunc.fetchBrand({name:row.brand},function(err,brands){
           if(err || !brands)
             return callback('Error while validating brand');
@@ -604,10 +608,17 @@ exports.bulkUpload = function(req, res) {
           if(!brands.length){
               row.otherBrand = row.brand;
               row.brand = "Other";
-              row.otherModel = row.model;
-              row.model = "Other";
+
+              if(row.model){
+                 row.otherModel = row.model;
+                 row.model = "Other";
+              }
+             
               return callback();
           } 
+
+          if(!row.model)
+            return callback();
 
           var modelParams = {
             brand : row.brand,
@@ -986,6 +997,7 @@ exports.bulkModify = function(req, res) {
           legalEntityName : (result[0].company || "")
         };
 
+        row.customerPartyName = row.enterprise.name;
         return callback();
       });
     }
@@ -1103,8 +1115,10 @@ exports.bulkModify = function(req, res) {
     }
 
      function validateMasterData(callback){
-      if(!row.brand)
-        return callback();
+      if(!row.brand){
+          delete row.model;
+          return callback();
+      }
        commonFunc.fetchBrand({name:row.brand},function(err,brands){
           if(err || !brands)
             return callback('Error while validating brand');
@@ -1112,10 +1126,15 @@ exports.bulkModify = function(req, res) {
           if(!brands.length){
               row.otherBrand = row.brand;
               row.brand = "Other";
-              row.otherModel = row.model;
-              row.model = "Other";
+              if(row.model){
+                row.otherModel = row.model;
+                row.model = "Other";
+                
+              }
               return callback();
           } 
+          if(!row.model)
+            return callback();
 
           var modelParams = {
             brand : row.brand,
@@ -1240,7 +1259,7 @@ exports.bulkModify = function(req, res) {
       var valReq = row.valData;
       delete row.valData;
       if(updateType == 'agency'){
-
+        row.reportSubmissionDate = new Date();
         row.status = EnterpriseValuationStatuses[4];
         row.statuses = valReq.statuses;
         row.statuses.push({
@@ -1341,6 +1360,8 @@ exports.update = function(req, res) {
   }
 
   function update(){
+    if(bodyData.status === EnterpriseValuationStatuses[4])
+        bodyData.reportSubmissionDate = new Date();
      EnterpriseValuation.update({_id:req.params.id},{$set:bodyData},function(err){
         if (err) { return handleError(res, err); }
         return res.status(200).json({errorCode:0, message:"Enterprise valuation updated sucessfully"});
@@ -1600,6 +1621,7 @@ exports.updateFromAgency = function(req,res){
 
       updateObj.status = EnterpriseValuationStatuses[4];
       updateObj.statuses = valReq.statuses;
+      updateObj.reportSubmissionDate = new Date();
       var stsObj = {};
       stsObj.createdAt = new Date();
       stsObj.userId = "IQVL";
@@ -1688,6 +1710,9 @@ exports.exportExcel = function(req,res){
     filter['enterprise.enterpriseId'] = queryParam.enterpriseId;
   if(queryParam.agencyId)
     filter['agency._id'] = queryParam.agencyId;
+  if (queryParam.userId)
+    filter["createdBy._id"] = queryParam.userId;
+
   if(queryParam.ids){
     var ids = queryParam.ids.split(',');
     filter['_id'] = {$in:ids};
