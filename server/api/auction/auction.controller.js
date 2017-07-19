b7236fa3d062ca8ae193c03794d4202897fe1a85
vinay.gupta <vinay.gupta@varaunited.com>
2017-07-19 15:53:08 +0530

    updated task T326
    Summary: VAT change to GST in asset bulkupload
    
    Test Plan: No
    
    Reviewers: subhash.patel
    
    Reviewed By: subhash.patel
    
    Maniphest Tasks: T326
    
    Differential Revision: http://dev.varaunited.net/D92

--- server/api/auction/auction.controller.js
+++ server/api/auction/auction.controller.js
@@ -319,7 +319,7 @@ exports.bulkUpload = function(req, res, next) {
     'Engine_No.': 'engineNo',
     'Asset_Location': 'city',
     'Original_Invoice': 'originalInvoice',
-    'VAT%': 'vatPercentage',
+    'GST%': 'vatPercentage',
     'Contact_Person': 'contactName',
     'Contact_No.': 'contactNumber',
     'Product_ID': 'productId',
@@ -369,7 +369,7 @@ exports.bulkUpload = function(req, res, next) {
         if(!obj.originalInvoice){
           obj.invioceDate = '';
         }
-
+        
         uploadData.push(obj);
       } 
     }
