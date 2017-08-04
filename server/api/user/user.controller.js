2b5fbe0a8bb7a7ad2b79c821427f04cf1e065127
imran <imran.zafar@bharatconnect.com>
2017-08-04 18:24:13 +0530

    done

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -156,17 +156,22 @@ var excelData = req.excelData;
     }
 
     function validateLegalEntity(callback){
-      if(row.userType !== 'Legal Entity')
-        return callback();
-
+      console.log("row",row);
+      if(row.userType === 'Legal Entity'){
         if(!row.company){
           errorList.push({
            Error:'Missing mandatory parameter : Legal_Entity_Name',
            rowCount:row.rowCount
           });
           return callback('Error');
-        }else
-          return callback();
+        }
+    }
+    else{
+      if(row.company){
+        delete row.company;  
+    }
+  }
+  return callback();
     }
 
     function validateEmailAddress(callback){
@@ -336,6 +341,13 @@ exports.createUserReq = function(req,res,next){
     data.updatedAt = new Date();
     data.agree=true;
     
+    if(data.userType === "Individual")
+       data.userType="individual";
+       if(data.userType === "Private Entrepreneur")
+       data.userType="private";
+       if(data.userType === "Legal Entity")
+       data.userType="legalentity";
+
     User.create(data,function(err,doc){
       if(err || !doc){
         req.errorList.push({
