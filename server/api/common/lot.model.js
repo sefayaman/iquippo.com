60f10d3c8f41051e6a6c556558b134102402ee45
imran <imran.zafar@bharatconnect.com>
2017-08-23 11:51:47 +0530

    lot master change

--- server/api/common/lot.model.js
+++ server/api/common/lot.model.js
@@ -4,15 +4,13 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var LotSchema = new Schema({
-  auctId:String,
+  userId:String,
   lotNumber:String,
   auctionId:String,
   assetId:String,
   assetDesc:String,
   startingPrice:Number,
   reservePrice:Number,
-  lastMintBid:String,
-  extendedTo:String,
   createdBy:{},
   createdAt: {type:Date,default:Date.now},
   updatedAt: {type:Date,default:Date.now}
