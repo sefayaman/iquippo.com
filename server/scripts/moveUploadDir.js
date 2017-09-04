b608e0b920b2ad2bc705dceeb5ceeaa6a6ffff50
Atul Agrawal <atul.agrawal@varaunited.com>
2017-09-04 22:57:31 +0530

    Minor bug fixes
    Summary: mapLimit used instead of eachLimit
    
    Test Plan: n/a
    
    Reviewers: jagdish.koli
    
    Reviewed By: jagdish.koli
    
    Differential Revision: http://dev.varadev.com/D118

--- server/scripts/moveUploadDir.js
+++ server/scripts/moveUploadDir.js
@@ -1,10 +1,15 @@
 'use strict';
 
 var fs = require('fs');
-var config = require('../config/environment/development');
+process.env.NODE_ENV = 'development';
+var config = require('../config/environment');
 var path = require('path');
 var utility = require('../components/utility.js'); // /server/components/utility.js
 var async = require('async');
+var _ = require('lodash');
+var rimraf = require('rimraf');
+var count = 0;
+var util = require('util');
 
 /*
 AA;Process of existing uploading images on s3
@@ -16,66 +21,91 @@ chunk size could be increased if we have more directories
 
 
 function init(cb) {
-	var uploadPath = config.templatePath;
-	fs.readdir(uploadPath, function(err, dir) {
-		if (err) {
-			console.log(err);
-			cb(err);
-		}
-
-		async.forEach(dir, initRead, endRead);
-
-		function initRead(x, outerCb) {
-			//Found uploads the push it in chunks
-			if (x === 'uploads') {
-				var newUploadPath = path.join(config.templatePath, x);
-				fs.readdir(newUploadPath, function(uploadErr, data) {
-					if (uploadErr) {
-						outerCb(err);
-					}
-					async.eachLimit(data, 10, init, final);
-					function init(d, innerCb) {
-						var uploadDirPath = path.join(newUploadPath,d);
-						console.log(uploadDirPath);
-						utility.uploadFileS3(uploadDirPath, d, function(err) {
-							if (err)
-								return innerCb(err);
-							return innerCb();
-						});
-					}
-
-					function final(err) {
-						if (err) {
-							return outerCb(err);
-						}
-						return outerCb();
-					}
-				});
-			} else {
-				//else upload that directory directly
-				utility.uploadDirToS3(uploadPath, function(err) {
-					if (err)
-						return outerCb(err);
-					return outerCb();
-				});
+	try {
+		var uploadPath = '/var/lib/jenkins/iquippo.com/client/assets';
+		//var uploadPath = path.resolve('../../' + config.templatePath);
+		//var uploadPath =  config.templatePath;
+		util.log(uploadPath);
+		fs.readdir(uploadPath, function(err, dir) {
+			if (err) {
+				util.log(err);
+				throw err;
 			}
+
+			async.forEach(dir, initRead, endRead);
+
+			function initRead(x, outerCb) {
+				//Found uploads the push it in chunks
+				if (x === 'uploads') {
+					var newUploadPath = path.join(uploadPath, x);
+
+					fs.readdir(newUploadPath, function(uploadErr, data) {
+						if (uploadErr) {
+							return outerCb(uploadErr);
+						}
+
+						var chunkedArray = _.chunk(data, 100);
+						var chunkId = 0;
+						async.eachSeries(chunkedArray, startUpload, endUpload);
+
+						function startUpload(cA, endCb) {
+							chunkId++;
+							util.log('Running Chunk ID# %d', chunkId);
+							setTimeout(function() {
+								if (cA && cA.length) {
+									cA.forEach(function(d, index) {
+										var uploadDirPath = path.join(newUploadPath, d);
+										utility.uploadFileS3(uploadDirPath, d, function(err) {
+											if (err) {
+												util.log(err);
+											} else {
+												util.log('Uploaded directory # %d for chunkId # %d with path %s ', index, chunkId, uploadDirPath);
+												util.log('Deleting uploaded directory');
+
+												rimraf(uploadDirPath, function(err, data) {
+													util.log(err || 'Deletind directory');
+												});
+											}
+										});
+									});
+									return endCb();
+								}
+							}, 60000);
+						}
+
+						function endUpload(err) {
+							if (err)
+								return outerCb(err);
+							util.log('Calling Outer Cb');
+							return outerCb();
+						}
+					});
+				}
+			}
+
+			function endRead(error) {
+				if (error)
+					throw error;
+				//return cb(error);
+				util.log('Calling Final Outer Func');
+				return cb();
+			}
+		});
+	} catch (e) {
+		count++;
+		if (count < 300) {
+			setTimeout(init(cb), 120000);
+		} else {
+			process.exit(1);
 		}
-
-		function endRead(error) {
-			if (error)
-				return cb(error);
-
-			return cb();
-		}
-	});
-
+	}
 }
 
 if (require.main === module) {
 	(function() {
 		init(function(err, res) {
 			if (err) {
-				console.log(err);
+				util.log(err);
 				return process.exit(1);
 			}
 			return process.exit(0);
