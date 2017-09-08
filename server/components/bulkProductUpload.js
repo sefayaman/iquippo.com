07d6b5505d08d50af2429dcec8877402d09c65ce
Atul Agrawal <atul.agrawal@varaunited.com>
2017-09-08 21:26:26 +0530

    Auction/Spare Uplaod updated

--- server/components/bulkProductUpload.js
+++ server/components/bulkProductUpload.js
@@ -4,8 +4,9 @@ var bulkProductUpload = {};
 
 var fs = require('fs');
 var fsExtra = require('fs.extra');
-// var gm = require('gm');
-var gm = require('gm').subClass({imageMagick: true});
+var gm = require('gm').subClass({
+  imageMagick: true
+});
 var AdmZip = require('adm-zip');
 var config = require('./../config/environment');
 var mongoose = require('mongoose');
@@ -20,150 +21,169 @@ var Model = require('./../api/model/model.model');
 var appNotificationCtrl = require('../api/appnotification/appnotification.controller');
 var utility = require('./utility');
 
-bulkProductUpload.commitProduct = function(taskData,cb){
-  debugger;
+bulkProductUpload.commitProduct = function(taskData, cb) {
   var filename;
   if (taskData && taskData.taskInfo && taskData.taskInfo.filename) {
     filename = taskData.taskInfo.filename;
   } else {
-    return cb(new Error('Invalid taskinfo/file'),taskData);
+    return cb(new Error('Invalid taskinfo/file'), taskData);
   }
   var zip = new AdmZip(config.uploadPath + "temp/" + filename);
   taskData.zip = zip;
   var zipEntries = zip.getEntries();
   var zipEntryObj = {};
-  zipEntries.forEach(function(zipEntry){
-    if(!zipEntry.isDirectory){
+  zipEntries.forEach(function(zipEntry) {
+    if (!zipEntry.isDirectory) {
       var entryName = zipEntry.entryName;
       var entryNameParts = entryName.split('/');
-        var assetId = entryNameParts[entryNameParts.length - 2];
-        if(!zipEntryObj[assetId]){
-          zipEntryObj[assetId] = [];
-        }
-        var obj = {};
-        obj.name = zipEntry.name;
-        obj.entryName = zipEntry.entryName;
-        zipEntryObj[assetId].push(obj);
-    }
-   });
-    var  keys = Object.keys(zipEntryObj);
-    taskData.uploadedProducts = [];
-    getProduct(keys,zipEntryObj,taskData,cb);
-}
-
-function getProduct(assetIds,zipEntryObj,taskData,cb){
-    debugger;
-    if(assetIds.length > 0){
-      var assetId = assetIds[0];
-      IncomingProduct.findOneAndUpdate({assetId:assetId,'user._id':taskData.user._id,lock:{$ne:true}},{ $set: {lock:true}},function(err,incPrd){
-          debugger;
-          if(err || !incPrd){
-            assetIds.splice(0,1);
-            getProduct(assetIds,zipEntryObj,taskData,cb);
-          }else{
-              extractEntryAndMapImages(assetIds,incPrd,zipEntryObj,taskData,cb);            
-          }
-      })      
-    }else{
-      try{
-        fs.unlink(config.uploadPath + "temp/" + taskData.taskInfo.filename);
-      }catch(e){
-        console.log('error in deleting file',e);
+      var assetId = entryNameParts[entryNameParts.length - 2];
+      if (!zipEntryObj[assetId]) {
+        zipEntryObj[assetId] = [];
       }
-      cb(true,taskData);
+      var obj = {};
+      obj.name = zipEntry.name;
+      obj.entryName = zipEntry.entryName;
+      zipEntryObj[assetId].push(obj);
     }
+  });
+  var keys = Object.keys(zipEntryObj);
+  taskData.uploadedProducts = [];
+  getProduct(keys, zipEntryObj, taskData, cb);
 }
 
-function extractEntryAndMapImages(assetIds,product,zipEntryObj,taskData,cb){
-  debugger;
-    var ret = false;
-   try{
-       product.images = [];
-       product.assetDir = new Date().getTime();
-       zipEntryObj[assetIds[0]].forEach(function(item,index){
-          if(item.name.match(/\.(jpg|jpeg|png)$/i) && index < 8){
-            taskData.zip.extractEntryTo(item.entryName,config.uploadPath + "/" + product.assetDir + "/" ,false);
-            var img = {};
-            img.isPrimary = false;
-            img.waterMarked = false;
-            img.src = item.name;
-            product.images.push(img);
-          }
-       });
-       if(product.images.length > 0){
-          product.primaryImg = product.images[0].src;
-          product.images[0].isPrimary = true;
-       }
-       //product.assetDir = product.assetId;
-       taskData.imgCounter = 0;
-    }catch(e){
-       IncomingProduct.update({_id:product._id},{$set:{lock:false}});
-       console.log("error in extracting.",e);
-       ret = true;
+function getProduct(assetIds, zipEntryObj, taskData, cb) {
+  if (assetIds.length > 0) {
+    var assetId = assetIds[0];
+    IncomingProduct.findOneAndUpdate({
+      assetId: assetId,
+      'user._id': taskData.user._id,
+      lock: {
+        $ne: true
+      }
+    }, {
+      $set: {
+        lock: true
+      }
+    }, function(err, incPrd) {
+      if (err || !incPrd) {
+        assetIds.splice(0, 1);
+        getProduct(assetIds, zipEntryObj, taskData, cb);
+      } else {
+        extractEntryAndMapImages(assetIds, incPrd, zipEntryObj, taskData, cb);
+      }
+    })
+  } else {
+    try {
+      fs.unlink(config.uploadPath + "temp/" + taskData.taskInfo.filename);
+    } catch (e) {
+      console.log('error in deleting file', e);
     }
-    if(ret){
-      assetIds.splice(0,1);
-      getProduct(assetIds,zipEntryObj,taskData,cb);
-      return;
-    }
-    placeWaterMark(assetIds,product,zipEntryObj,taskData,cb);
+    cb(true, taskData);
+  }
 }
 
-function placeWaterMark(assetIds,product,zipEntryObj,taskData,cb){
-    debugger;
-    if(taskData.imgCounter < product.images.length){
-      var waterMarkWidth = 144;
-      var waterMarkHeight = 86;
-      try{
-        var imgPath = config.uploadPath + product.assetDir + "/" + product.images[taskData.imgCounter].src;
-        var imgRef = gm(imgPath);
-        imgRef.size(function(err,val){
-          if(err){
-             console.log("error in getting detail",err);
-             taskData.imgCounter ++;
-             placeWaterMark(assetIds,product,zipEntryObj,taskData,cb);
-          }else{
-             var ptx = val.width - waterMarkWidth;
-            var pty = val.height - waterMarkHeight;
-            var args = 'image Over '+ ptx +','+ pty  +' 0,0 "' + config.uploadPath + '../images/watermark.png"';
-            imgRef.draw([args])
-            .write(imgPath, function(e){
-              if(e){
-                console.log('Error in placing watermark',e);  
-              }else
+function extractEntryAndMapImages(assetIds, product, zipEntryObj, taskData, cb) {
+  var ret = false;
+  try {
+    product.images = [];
+    product.assetDir = new Date().getTime();
+    zipEntryObj[assetIds[0]].forEach(function(item, index) {
+      if (item.name.match(/\.(jpg|jpeg|png)$/i) && index < 8) {
+        taskData.zip.extractEntryTo(item.entryName, config.uploadPath + "/" + product.assetDir + "/", false);
+        var img = {};
+        img.isPrimary = false;
+        img.waterMarked = false;
+        img.src = item.name;
+        product.images.push(img);
+      }
+    });
+    if (product.images.length > 0) {
+      product.primaryImg = product.images[0].src;
+      product.images[0].isPrimary = true;
+    }
+    //product.assetDir = product.assetId;
+    taskData.imgCounter = 0;
+  } catch (e) {
+    IncomingProduct.update({
+      _id: product._id
+    }, {
+      $set: {
+        lock: false
+      }
+    });
+    console.log("error in extracting.", e);
+    ret = true;
+  }
+  if (ret) {
+    assetIds.splice(0, 1);
+    getProduct(assetIds, zipEntryObj, taskData, cb);
+    return;
+  }
+  placeWaterMark(assetIds, product, zipEntryObj, taskData, cb);
+}
+
+function placeWaterMark(assetIds, product, zipEntryObj, taskData, cb) {
+
+  if (taskData.imgCounter < product.images.length) {
+    var waterMarkWidth = 144;
+    var waterMarkHeight = 86;
+    try {
+      var imgPath = config.uploadPath + product.assetDir + "/" + product.images[taskData.imgCounter].src;
+      var imgRef = gm(imgPath);
+      imgRef.size(function(err, val) {
+        if (err) {
+          console.log("error in getting detail", err);
+          taskData.imgCounter++;
+          placeWaterMark(assetIds, product, zipEntryObj, taskData, cb);
+        } else {
+          var ptx = val.width - waterMarkWidth;
+          var pty = val.height - waterMarkHeight;
+          var args = 'image Over ' + ptx + ',' + pty + ' 0,0 "' + config.uploadPath + '../images/watermark.png"';
+          imgRef.draw([args])
+            .write(imgPath, function(e) {
+              if (e) {
+                console.log('Error in placing watermark', e);
+              } else
                 product.images[taskData.imgCounter].waterMarked = true;
-              taskData.imgCounter ++;
-              placeWaterMark(assetIds,product,zipEntryObj,taskData,cb);
+              taskData.imgCounter++;
+              placeWaterMark(assetIds, product, zipEntryObj, taskData, cb);
             });
-          }
-        })
-      }catch(e){
-        console.log("exception------",e);
-        taskData.imgCounter ++;
-        placeWaterMark(assetIds,product,zipEntryObj,taskData,cb);
-      }
-    }else{
-        var localFilePath = config.uploadPath + product.assetDir;
-      var dirName = product.assetDir;
-      console.log('Found some file');
-      utility.uploadFileS3(localFilePath,dirName,function(err,data){
-        debugger;
-          if(err){
-          console.log(err)
-          return cb(true,taskData);
         }
-            return commitProduct(assetIds,product,zipEntryObj,taskData,cb);
-        });
+      })
+    } catch (e) {
+      console.log("exception------", e);
+      taskData.imgCounter++;
+      placeWaterMark(assetIds, product, zipEntryObj, taskData, cb);
     }
-     
+  } else {
+    var localFilePath = config.uploadPath + product.assetDir;
+    var dirName = product.assetDir;
+    utility.uploadFileS3(localFilePath, dirName, function(err, data) {
+
+      if (err) {
+        console.log(err)
+        return cb(true, taskData);
+      }
+      return commitProduct(assetIds, product, zipEntryObj, taskData, cb);
+    });
+  }
+
 }
 
-function commitProduct(assetIds,product,zipEntryObj,taskData,cb){
-    debugger;
-    IncomingProduct.remove({_id:product._id},function(err,dt){
-    if(err){
-        IncomingProduct.update({_id:product._id},{$set:{lock:false}});
-        console.log("error in deleting product")
+function commitProduct(assetIds, product, zipEntryObj, taskData, cb) {
+
+  IncomingProduct.remove({
+    _id: product._id
+  }, function(err, dt) {
+    if (err) {
+      IncomingProduct.update({
+        _id: product._id
+      }, {
+        $set: {
+          lock: false
+        }
+      });
+      console.log("error in deleting product")
     }
     delete product._id;
     product.createdAt = new Date();
@@ -171,101 +191,127 @@ function commitProduct(assetIds,product,zipEntryObj,taskData,cb){
     product.relistingDate = new Date();
 
     Product.create(product, function(err, prd) {
-        debugger;
-      if(err){console.log('error in creating product')}
-      if(!err) taskData.uploadedProducts.push(product.assetId);
+
+      if (err) {
+        console.log('error in creating product')
+      }
+      if (!err) taskData.uploadedProducts.push(product.assetId);
       //create app notificaton data
-       appNotificationCtrl.createAppNotification(product);
-    
-       assetIds.splice(0,1);
-       getProduct(assetIds,zipEntryObj,taskData,cb);
+      appNotificationCtrl.createAppNotification(product);
+
+      assetIds.splice(0, 1);
+      getProduct(assetIds, zipEntryObj, taskData, cb);
     });
   })
 }
 
-bulkProductUpload.updateProductMaster = function(req,res){
-  Model.find({'group.name':"Other",'category.name':"Other",'brand.name':"Other",name:"Other"},function(err,models){
-    if(err){return res.status(500).send(err); }
-    if(models.length == 0)
+bulkProductUpload.updateProductMaster = function(req, res) {
+  Model.find({
+    'group.name': "Other",
+    'category.name': "Other",
+    'brand.name': "Other",
+    name: "Other"
+  }, function(err, models) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (models.length == 0)
       return res.status(404).send("Other model not found");
-    if(models.length > 1)
+    if (models.length > 1)
       return res.status(404).send("More than one model found");
-    var query = Product.find({$or:[{"brand.name":/^Other/i},{"category.name":/^Other/i},{"model.name":/^Other/i}],deleted:false});
-    query.exec(function(err,products){
-       if(err){return res.status(500).send(err); }
-       req.counter = 0;
-       req.total = products.length;
-       req.model = models[0];
-       updateProducts(req,res,products);
+    var query = Product.find({
+      $or: [{
+        "brand.name": /^Other/i
+      }, {
+        "category.name": /^Other/i
+      }, {
+        "model.name": /^Other/i
+      }],
+      deleted: false
+    });
+    query.exec(function(err, products) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      req.counter = 0;
+      req.total = products.length;
+      req.model = models[0];
+      updateProducts(req, res, products);
     })
 
   })
 }
 
-function updateProducts(req,res,products){
-  if(req.counter < req.total){
+function updateProducts(req, res, products) {
+  if (req.counter < req.total) {
     var prd = products[req.counter];
     var id = prd._id;
     delete prd._id;
     var flag = -1;
-    if(prd.category.name == "Others" || prd.category.name == "Other")
+    if (prd.category.name == "Others" || prd.category.name == "Other")
       flag = 1;
-    else if(prd.brand.name == "Others" || prd.brand.name == "Other")
+    else if (prd.brand.name == "Others" || prd.brand.name == "Other")
       flag = 2;
-    else if(prd.model.name == "Others" || prd.model.name == "Other")
+    else if (prd.model.name == "Others" || prd.model.name == "Other")
       flag = 3;
-    else{
-       req.counter ++;
-       updateProducts(req,res,products);
-       return;
+    else {
+      req.counter++;
+      updateProducts(req, res, products);
+      return;
     }
 
-    switch(flag){
+    switch (flag) {
       case 1:
         prd.category._id = req.model.category._id;
         prd.category.name = req.model.category.name;
-        if(!prd.category.otherName)
+        if (!prd.category.otherName)
           prd.category.otherName = "Others";
         prd.group = req.model.group;
       case 2:
         var brName = prd.brand.name;
         prd.brand._id = req.model.brand._id;
         prd.brand.name = req.model.brand.name;
-        if(!prd.brand.otherName)
-          prd.brand.otherName = brName!="Others" && brName!="Other"?brName:"Others";
+        if (!prd.brand.otherName)
+          prd.brand.otherName = brName != "Others" && brName != "Other" ? brName : "Others";
       case 3:
         var mdName = prd.model.name;
         prd.model._id = req.model._id;
         prd.model.name = req.model.name;
-        if(!prd.model.otherName)
-          prd.model.otherName = mdName!="Others" && mdName!="Other"?mdName:"Others";
-      break;
+        if (!prd.model.otherName)
+          prd.model.otherName = mdName != "Others" && mdName != "Other" ? mdName : "Others";
+        break;
     }
 
-    if(prd.category.name == "Other")
+    if (prd.category.name == "Other")
       prd.name = prd.category.otherName || "";
     else
       prd.name = prd.category.name || "";
 
-    if(prd.brand.name == "Other")
-      prd.name += " " +  prd.brand.otherName || "";
+    if (prd.brand.name == "Other")
+      prd.name += " " + prd.brand.otherName || "";
     else
       prd.name += " " + prd.brand.name || "";
 
-    if(prd.model.name == "Other")
+    if (prd.model.name == "Other")
       prd.name += " " + prd.model.otherName || "";
     else
-      prd.name += " " +  prd.model.name || "";
-    if(prd.variant)
+      prd.name += " " + prd.model.name || "";
+    if (prd.variant)
       prd.name += " " + prd.variant;
-    
-    Product.update({_id:id},{$set:prd},function(err,data){
-      if(err){console.log("###",err)}
-        req.counter ++;
-        updateProducts(req,res,products);
+
+    Product.update({
+      _id: id
+    }, {
+      $set: prd
+    }, function(err, data) {
+      if (err) {
+        console.log("###", err)
+      }
+      req.counter++;
+      updateProducts(req, res, products);
     })
 
-  }else{
+  } else {
     return res.status(200).send("Done");
   }
 }
--- server/components/bulkUpload.js
+++ server/components/bulkUpload.js
@@ -4,13 +4,22 @@ var fs = require('fs');
 
 var AdmZip = require('adm-zip');
 var config = require('./../config/environment');
+var mongoose = require('mongoose');
+mongoose.createConnection(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+	console.error('MongoDB connection error: ' + err);
+	process.exit(-1);
+});
+
 var UploadRequestModel = require('../api/common/uploadrequest/uploadrequest.model');
 var AuctionController = require('../api/auction/auction.controller');
 var APIError = require('./_error');
 var async = require('async');
 var moment = require('moment');
 var validDateFormat = ['DD/MM/YYYY', 'MM/DD/YYYY', 'YYYY/MM/DD'];
-var spareController = require('../api/spare/spare.controller')
+var spareController = require('../api/spare/spare.controller');
+var utility = require('./utility');
+
 
 var bulkUpload = {};
 
@@ -32,6 +41,18 @@ function _fetchRequestData(options, cb) {
 
 		return cb(null, requests);
 	}
+}
+
+function _uploadToS3(opts, cb) {
+	var localFilePath = config.uploadPath + opts.assetDir;
+	var dirName = opts.assetDir;
+	utility.uploadFileS3(localFilePath, dirName, function(err, data) {
+		if (err) {
+			console.log(err)
+			return cb(err);
+		}
+		return cb();
+	});
 }
 
 function _extractImages(taskData) {
@@ -108,11 +129,13 @@ bulkUpload.init = function(taskData, next) {
 					return next('Error while uploading images', taskData);
 				}
 
-				data.forEach(function(x) {
+				async.eachLimit(data, 1, iterate, finish);
+
+				function iterate(x, cb) {
 					var product = x.product;
 					var obj = {};
 					product.images = [];
-					product.assetDir = "/auction/" + new Date().getTime();
+					product.assetDir = "auction/" + new Date().getTime();
 					if (imagesObj[product.assetId] && imagesObj[product.assetId].length) {
 						obj = {};
 						for (var i = 0; i < imagesObj[product.assetId].length; i++) {
@@ -126,120 +149,99 @@ bulkUpload.init = function(taskData, next) {
 								product.otherImages.push(imagesObj[product.assetId][j].name)
 							}
 						}
-						/*product.isSold = product.isSold ? true : false;
-						if (product.isSold) {
-							product.saleVal = Number(product.saleVal);
-						}
-						product.originalInvoice = product.originalInvoice ? "Yes" : "No";
-
-						if (product.originalInvoice === "Yes") {
-							product.invoiceDate = (moment(product.invioceDate, validDateFormat).format('MM/DD/YYYY'))
-						} else {
-							delete product.invoiceDate;
-						}
-
-						if (product.contactNumber) {
-							product.contactNumber = Number(product.contactNumber);
-						}
-
-						if (product.vatPercentage) {
-							product.vatPercentage = Number(product.vatPercentage);
-						}
-
-						obj.product = product;
-						obj.user = x.user;
-						obj.dbAuctionId = x.auction.dbAuctionId;
-						obj.lotNo = x.lotNo;
-						obj.auctionId = x.auction.auctionId;
-						obj.startDate = x.auction.startDate;
-						obj.endDate = x.auction.endDate;
-						obj.external = true;
-						obj.statuses = [{
-							createdAt: new Date(),
-							status: x.status,
-							userId: x.user._id
-						}];
-						obj.status = 'request_approved';
-						obj.external = true;
-						uploadedProducts.push(product.assetId);
-						approvedObj.push(obj);
-						approvedIds.push(x._id.toString());*/
 					}
-						product.isSold = product.isSold ? true : false;
-						if (product.isSold) {
-							product.saleVal = Number(product.saleVal);
-						}
-						product.originalInvoice = product.originalInvoice ? "Yes" : "No";
-					
-						if (product.originalInvoice === "Yes") {
-							if(!product.invioceDate){
-								delete product.invoiceDate;
-							}else{
-								product.invoiceDate = (moment(product.invioceDate, validDateFormat).format('MM/DD/YYYY'));
-							}
-						} else {
+
+					product.isSold = product.isSold ? true : false;
+					if (product.isSold) {
+						product.saleVal = Number(product.saleVal);
+					}
+
+					product.originalInvoice = product.originalInvoice ? "Yes" : "No";
+
+					if (product.originalInvoice === "Yes") {
+						if (!product.invioceDate) {
 							delete product.invoiceDate;
+						} else {
+							product.invoiceDate = (moment(product.invioceDate, validDateFormat).format('MM/DD/YYYY'));
 						}
+					} else {
+						delete product.invoiceDate;
+					}
 
-						if (product.contactNumber) {
-							product.contactNumber = Number(product.contactNumber);
-						}
+					if (product.contactNumber) {
+						product.contactNumber = Number(product.contactNumber);
+					}
 
-						if (product.vatPercentage) {
-							product.vatPercentage = Number(product.vatPercentage);
-						}
+					if (product.vatPercentage) {
+						product.vatPercentage = Number(product.vatPercentage);
+					}
 
-						obj.product = product;
-						obj.user = x.user;
-						obj.dbAuctionId = x.auction.dbAuctionId;
-						obj.lotNo = x.lotNo;
-						obj.auctionId = x.auction.auctionId;
-						obj.startDate = x.auction.startDate;
-						obj.endDate = x.auction.endDate;
-						obj.external = true;
-						obj.statuses = [{
-							createdAt: new Date(),
-							status: x.status,
-							userId: x.user._id
-						}];
-						obj.status = 'request_approved';
-						obj.external = true;
-						uploadedProducts.push(product.assetId);
-						approvedObj.push(obj);
-						approvedIds.push(x._id.toString());
-				})
+					obj.product = product;
+					obj.user = x.user;
+					obj.dbAuctionId = x.auction.dbAuctionId;
+					obj.lotNo = x.lotNo;
+					obj.auctionId = x.auction.auctionId;
+					obj.startDate = x.auction.startDate;
+					obj.endDate = x.auction.endDate;
+					obj.external = true;
+					obj.statuses = [{
+						createdAt: new Date(),
+						status: x.status,
+						userId: x.user._id
+					}];
+					obj.status = 'request_approved';
+					obj.external = true;
+					uploadedProducts.push(product.assetId);
+					approvedObj.push(obj);
+					approvedIds.push(x._id.toString());
+					var opts = {
+						assetDir: product.assetDir
+					};
 
-				var rejectIds = [];
-				if (approvedObj.length) {
-					async.eachLimit(approvedIds, 5, iterator, finalize);
-				} else {
-					return next(false, taskData);
-				}
-
-
-				function iterator(approveId, cb) {
-					UploadRequestModel.remove({
-						_id: approveId
-					}, function(err, dt) {
+					_uploadToS3(opts, function(err, data) {
 						if (err) {
 							console.log(err);
-							rejectIds.push(approveId);
+							return cb(err);
 						}
 						return cb();
-					})
+					});
 				}
 
-				function finalize(err) {
-					console.log(err);
-					AuctionController.bulkCreate(approvedObj, function(err, response) {
-						taskData.data = data;
-						if (response && !response.Error && !response.errObj && response.sucessObj) {
-							taskData.uploadedProducts = uploadedProducts;
-							return next(true, taskData);
-						} else {
-							return next(false, taskData);
-						}
-					})
+				function finish(err) {
+					if (err)
+						return next(false, taskData);
+
+					var rejectIds = [];
+					if (approvedObj.length) {
+						async.eachLimit(approvedIds, 5, iterator, finalize);
+					} else {
+						return next(false, taskData);
+					}
+
+					function iterator(approveId, cb) {
+						UploadRequestModel.remove({
+							_id: approveId
+						}, function(err, dt) {
+							if (err) {
+								console.log(err);
+								rejectIds.push(approveId);
+							}
+							return cb();
+						})
+					}
+
+					function finalize(err) {
+						console.log(err);
+						AuctionController.bulkCreate(approvedObj, function(err, response) {
+							taskData.data = data;
+							if (response && !response.Error && !response.errObj && response.sucessObj) {
+								taskData.uploadedProducts = uploadedProducts;
+								return next(true, taskData);
+							} else {
+								return next(false, taskData);
+							}
+						})
+					}
 				}
 			})
 			break;
@@ -260,8 +262,9 @@ bulkUpload.init = function(taskData, next) {
 				if (imagesObj instanceof Error) {
 					return next('Error while uploading images', taskData);
 				}
+				async.eachLimit(data, 1, iterate, finish);
 
-				data.forEach(function(x) {
+				function iterate(x, cb) {
 					var spareUploads = x.spareUploads;
 					var obj = {};
 					spareUploads.images = [];
@@ -291,39 +294,57 @@ bulkUpload.init = function(taskData, next) {
 						spareUploaded.push(spareUploads.partNo);
 						approvedIds.push(x._id.toString());
 						approvedObj.push(spareUploads);
+
+						var opts = {
+							assetDir: spareUploads.assetDir
+						};
+
+						_uploadToS3(opts, function(err, data) {
+							if (err) {
+								console.log(err);
+								return cb(err);
+							}
+							return cb();
+						})
 					}
-				})
-				var rejectIds = [];
-				if (approvedObj.length) {
-					async.eachLimit(approvedIds, 5, iterator, finalize);
-				} else {
-					return next(false, taskData);
 				}
 
+				function final(err) {
+					if (err)
+						return next(false, taskData);
 
-				function iterator(approveId, cb) {
-					UploadRequestModel.remove({
-						_id: approveId
-					}, function(err, dt) {
-						if (err) {
-							console.log(err);
-							rejectIds.push(approveId);
-						}
-						return cb();
-					})
-				}
+					var rejectIds = [];
+					if (approvedObj.length) {
+						async.eachLimit(approvedIds, 5, iterator, finalize);
+					} else {
+						return next(false, taskData);
+					}
 
-				function finalize(err) {
-					console.log(err);
-					spareController.bulkCreate(approvedObj, function(err, response) {
-						taskData.data = data;
-						if (response && !response.Error && !response.errObj && response.sucessObj) {
-							taskData.spareUploaded = spareUploaded;
-							return next(true, taskData);
-						} else {
-							return next(false, taskData);
-						}
-					})
+
+					function iterator(approveId, cb) {
+						UploadRequestModel.remove({
+							_id: approveId
+						}, function(err, dt) {
+							if (err) {
+								console.log(err);
+								rejectIds.push(approveId);
+							}
+							return cb();
+						})
+					}
+
+					function finalize(err) {
+						console.log(err);
+						spareController.bulkCreate(approvedObj, function(err, response) {
+							taskData.data = data;
+							if (response && !response.Error && !response.errObj && response.sucessObj) {
+								taskData.spareUploaded = spareUploaded;
+								return next(true, taskData);
+							} else {
+								return next(false, taskData);
+							}
+						})
+					}
 				}
 			})
 			break;
