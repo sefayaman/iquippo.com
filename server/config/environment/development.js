fe93de6295222ab10f42695b87433475754ac91a
jagdish.koli <jagdish.koli@varaunited.com>
2017-09-28 17:08:32 +0530

    Fixed task T415: Image not displayed on email

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -9,13 +9,13 @@ module.exports = {
   },
   uploadPath: 'client/assets/uploads/',
   templatePath:'client/assets',
-  //serverPath: "http://192.168.14.120", /*Commented for email templete images from s3 URL*/
-  serverPath: "https://s3.ap-south-1.amazonaws.com/iquippo-image-upload-dev", 
+  //serverPath: "http://192.168.14.120", 
+  serverPath: "http://localhost:8100", 
   contactNumber: "011 66025672",
   ccAvenueWorkingKey:"BCCD36E2D20659D5F76B99973880340D",
 
   seedDB: true,
-  mailConfig : { MailHost: "smtp.gmail.com", MailPort: "465", Mailusername: "iquippo.uat@gmail.com", Mailpassword: "pass12345678",from:"info@iquippo.com"},
+  mailConfig : { MailHost: "smtp.gmail.com", MailPort: "465", Mailusername: "iquippo.uat@gmail.com", Mailpassword: "pass12345678",from:"iquippo.uat@gmail.com"},
   qpvalURL:"http://13.126.19.255/valuation/api.php?type=Mjobcreation",
   REDIRECT_URL:"http://finance-uat.iquippo.com/customer-portal-iquippo/sso",
 
