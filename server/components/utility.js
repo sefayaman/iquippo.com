8fc2ec86e523817dded76a6fcc468e811931fff4
imran <imran.zafar@bharatconnect.com>
2017-10-13 12:35:49 +0530

    model added

--- server/components/utility.js
+++ server/components/utility.js
@@ -171,16 +171,16 @@ function sendCompiledData(options, cb) {
   console.log("totalData", options);
   //filter._id = emdDataAs.auctionId
   //console.log(options.dataToSend, options.dataType)
-   if(options.dataToSend.__v === 0)
+   if(options.dataToSend.hasOwnProperty('__v'))
     delete options.dataToSend.__v;
  /* if (options.dataToSend.auctionId && options.dataType!=="auctionData"){
     filter._id = options.dataToSend.auctionId;
   }*/
-  if (options.dataToSend.startDate && options.dataToSend.endDate) {
+  if (options.dataToSend.hasOwnProperty('startDate') && options.dataToSend.hasOwnProperty('endDate')) {
     options.dataToSend.startDate = options.dataToSend.startDate.toString();
     options.dataToSend.endDate = options.dataToSend.endDate.toString();
   }
-  if (options.dataToSend && options.dataToSend.updatedAt &&  options.dataToSend.createdAt){
+  if (options.dataToSend.hasOwnProperty('updatedAt') &&  options.dataToSend.hasOwnProperty('createdAt')){
     delete options.dataToSend.updatedAt;
     delete options.dataToSend.createdAt;
   }
@@ -206,11 +206,8 @@ function compileData(options, callback) {
       break;
     case "lotData":
       // dataFormat = "lots";
-      if (options.dataToSend.assetDir)
+      if (options.dataToSend.hasOwnProperty('assetDir'))
         delete options.dataToSend.assetDir;
-      if (!options.dataToSend.emdAmount)
-        options.dataToSend.emdAmount = "";
-
        callback(null, options);
       break;
     case "emdData":
@@ -222,9 +219,13 @@ function compileData(options, callback) {
       if(isEmpty(options.dataToSend.bidIncrement)){
         delete options.dataToSend.bidIncrement;
       }
-      if(options.dataToSend.bidInfo)
+      if(options.dataToSend.hasOwnProperty('staticIncrement'))
+        delete options.dataToSend.staticIncrement;
+      if(options.dataToSend.hasOwnProperty('rangeIncrement'))
+        delete options.dataToSend.rangeIncrement;
+      if(options.dataToSend.hasOwnProperty('bidInfo'))
         delete options.dataToSend.bidInfo;
-      if (options.dataToSend.auctionOwner && options.dataToSend.auctionOwnerMobile) {
+      if (options.dataToSend.hasOwnProperty('auctionOwner') && options.dataToSend.hasOwnProperty('auctionOwnerMobile')) {
         delete options.dataToSend.auctionOwner;
         delete options.dataToSend.auctionOwnerMobile;
       }
