8d2fe958961491a8223b88e4d7209b129de691d2
subhash <subhash.patel@bharatconnect.com>
2017-11-21 04:52:52 +0530

    duplicate brand issue fixes

--- client/app/filters.js
+++ client/app/filters.js
@@ -8,4 +8,22 @@ angular.module('sreizaoApp')
         	return; 
         return input.slice(start);
     };
+})
+.filter('unique', function() {
+
+  return function (arr, field) {
+  	if(!arr || !arr.length || !field)
+  		return arr;
+
+  	var cache = {};
+  	var unqueArr = [];
+  	for(var i = 0; i< arr.length; i++){
+  		var keyVal = _.get(arr[i],field,"");
+  		if(!cache[keyVal] && keyVal){
+  			unqueArr.push(arr[i]);
+  			cache[keyVal] = 1;
+  		}
+  	}
+  	return unqueArr;
+  };
 });
