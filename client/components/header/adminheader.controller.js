81a53e5ab6e9a10fa68083f73da0939eb6c0f32b
Narendra <narendra.khatri@varaunited.com>
2017-12-10 20:35:42 +0530

    comment url

--- client/components/header/adminheader.controller.js
+++ client/components/header/adminheader.controller.js
@@ -3,6 +3,5 @@
 angular.module('sreizaoApp')
   .controller('AdminHeaderCtrl', function ($state, $scope, $rootScope, $http, $location, Auth,$uibModal,Modal,notificationSvc) {
     $rootScope.getCurrentUser = Auth.getCurrentUser;
-    $scope.url="https://auctionsoftwaremarketplace.com:3007/buyes/"+Auth.getCurrentUser()._id;
-    console.log("url",$scope.url);
+    //$scope.url="https://auctionsoftwaremarketplace.com:3007/buyer/"+Auth.getCurrentUser()._id;
   })
