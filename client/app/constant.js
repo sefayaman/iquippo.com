be237a0a1caadafb97d236f34b8c5f236a036bd0
jagdish.koli <jagdish.koli@varaunited.com>
2017-12-18 17:40:41 +0530

    newsEvents print media links update

--- client/app/constant.js
+++ client/app/constant.js
@@ -561,11 +561,13 @@ informationMessage['invoiceUpdateForThirdParty'] = "Invoice in third party's nam
 var newsEvents = [
   {
     name:"The Economic Times",
-    image:"The_Economic_Times.jpg"
+    //image:"The_Economic_Times.jpg"
+    image:"The_Economic_Times.PNG"
   },
   {
     name:"The Hindu Business Line",
-    image:"The_Hindu_Business_Line.jpg"
+    //image:"The_Hindu_Business_Line.jpg"
+    image:"The_Hindu_Business_Line_1.jpg"
   },
   {
     name:"The Financial Express",
