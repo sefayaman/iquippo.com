012c0f5f5c2278ce3d3e6996487dc37ee7fe7ba9
jagdish.koli <jagdish.koli@varaunited.com>
2018-01-17 17:45:15 +0530

    T565 : Change in Ticket ID for Buy Now Report

---
+++ server/scripts/updateBuynowReports.js
@@ -0,0 +1,74 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var _ = require('lodash');
+var Negotiation = require('./../api/negotiation/negotiation.model');
+var config = require('./../config/environment');
+var async = require("async");
+var util = require('util');
+var mongoose = require('mongoose');
+
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function (err) {
+    util.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+});
+
+function init(processCb) {
+    findSB();
+    findGN();
+    findBN();
+    function findSB() {
+        Negotiation.find({ticketId: /^SB/}, function (err, reports) {
+            if (err)
+                return processCb(err);
+            async.eachLimit(reports, 3, processBids, function (err, result) {
+                return processCb();
+            });
+        });
+    }
+    function findGN() {
+        Negotiation.find({ticketId: /^GN/}, function (err, reports) {
+            if (err)
+                return processCb(err);
+            async.eachLimit(reports, 3, processBids, function (err, result) {
+                return processCb();
+            });
+        });
+    }
+    function findBN() {
+        Negotiation.find({ticketId: /^BN/}, function (err, reports) {
+            if (err)
+                return processCb(err);
+            async.eachLimit(reports, 3, processBids, function (err, result) {
+                return processCb();
+            });
+        });
+    }
+
+    function processBids(nego, cb) {
+        var old = 'O';
+        var TicketId = old + nego.ticketId;
+        console.log('Ticket ID: ', nego.ticketId);
+        Negotiation.update({ticketId: nego.ticketId}, {$set: {ticketId: TicketId}}, function (error, resultData) {
+            if (error) {
+                console.log("##########", error);
+            }
+            return cb();
+        });
+    }
+
+}
+
+if (require.main === module) {
+    console.log("Started At:-- " + new Date());
+    (function () {
+        init(function (err, errList) {
+            if (err) {
+                util.log(err);
+                return process.exit(1);
+            }
+            console.log("Done without error:-- " + new Date());
+            return process.exit(0);
+        });
+    }());
+}
