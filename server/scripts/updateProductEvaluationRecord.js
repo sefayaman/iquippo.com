6a6ba910927f35ede3638be6b043187bba141d04
Maxgrid <mohit.khalkho@varaunited.com>
2018-03-14 13:17:18 +0530

    Modification in update query

--- server/scripts/updateProductEvaluationRecord.js
+++ server/scripts/updateProductEvaluationRecord.js
@@ -25,7 +25,7 @@ function init(processCb) {
                     valuationAssessedValue: entrpriseVal.assessedValue,
                     valuationOverallGeneralCondition: entrpriseVal.overallGeneralCondition
                 };
-                Product.update({assetId: entrpriseVal.assetId}, data , function (err, res) {
+                Product.update({assetId: entrpriseVal.assetId}, {$set:data} , function (err, res) {
                     if (err) {
                         cb();
                     } else {
